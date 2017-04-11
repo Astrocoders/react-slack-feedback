54604483dab3fb5182b4723dcb05bb01cf2867d2
Yuri Jean Fabris <yuri.fabris@gmail.com>
2017-04-11 17:04:42 -0300

    Add reCAPTCHA, form validation and minor adjustments

--- src/SlackFeedback.js
+++ src/SlackFeedback.js
@@ -1,5 +1,4 @@
 import React, { Component, PropTypes } from 'react';
-import reCAPTCHA from 'react-recaptcha';
 import Helmet from 'react-helmet';
 
 // classnames
@@ -20,7 +19,8 @@ const propTypes = {
   buttonText: PropTypes.string,
   imageUploadText: PropTypes.string,
   triggerStyles: PropTypes.object,
-  contentStyles: PropTypes.object
+  contentStyles: PropTypes.object,
+  sitekey: PropTypes.string
 };
 
 const defaultProps = {
@@ -52,252 +52,22 @@ class SlackFeedback extends Component {
       error: false,
       uploadingImage: false,
       selectedType: 'Bug',
-      image: {}
-    };
-
-    // Bind event handlers once to avoid performance issues with re-binding
-    // on every render
-    this.removeImage = this.removeImage.bind(this);
-    this.toggle = this.toggle.bind(this);
-    this.send = this.send.bind(this);
-    this.toggleSendURL = this.toggleSendURL.bind(this);
-    this.selectType = this.selectType.bind(this);
-    this.close = this.close.bind(this);
-  }
-
-  toggle() {
-    if (this.state.active) {
-      this.close();
-    } else {
-      this.activate();
-    }
-  }
-
-  activate() {
-    this.setState({
-      active: !this.state.active
-    });
-
-    document.addEventListener('click', this.handleClickOutside.bind(this));
-  }
-
-  handleClickOutside(event) {
-
-    if (event.defaultPrevented) return;
-
-    if (!this.refs.SlackFeedback.contains(event.target)) {
-      this.close();
-    }
-  }
-
-  close() {
-    this.setState({
-      active: false
-    });
-
-    document.removeEventListener('click', this.handleClickOutside.bind(this));
-  }
-
-  toggleSendURL() {
-    this.setState({
-      sendURL: !this.state.sendURL
-    });
-  }
-
-  selectType(e) {
-    this.setState({
-      selectedType: e.target.innerText
-    });
-  }
-
-  sent() {
-    this.setState({
-      sending: false,
-      sent: true,
       image: {},
-      error: false,
-    }, () => {
-      this.refs.message.value = '';
-      setTimeout(() => {
-        this.setState({ sent: false });
-      }, 5 * 1000);
-    });
-  }
-
-  error(err) {
-    this.setState({
-      sending: false,
-      error: this.determineErrorType(err)
-    }, () => {
-
-      setTimeout(() => {
-        this.setState({ error: null })
-      }, 8 * 1000);
-    });
-  }
-
-  determineErrorType(err) {
-    if (!err || typeof err !== 'string') return 'Unexpected Error!';
-
-    switch(err.status) {
-      case 400: return 'Bad Request!';
-      case 403: return 'Forbidden!';
-      case 404: return 'Channel Not Found!';
-      case 410: return 'Channel is Archived!';
-      case 500: return 'Server Error!';
-      default: return 'Unexpected Error!';
-    }
-  }
-
-  send() {
-    var { selectedType, sendURL, image } = this.state;
-    var message = this.refs.message.value;
-    var level;
-
-    this.setState({
-      sending: true
-    });
-
-    // Attach the curent URL
-    if (sendURL) message += `\n <${document.location.href}>`;
-
-    // Slack accepts 3 color levels: danger (red), good (green) and warning (orange)
-    switch (selectedType) {
-      case 'Bug':
-        level = 'danger';
-        break;
-      case 'Feature':
-        level = 'good';
-        break;
-      case 'Improvement':
-        level = 'warning';
-        break;
-    }
-
-    var payload = {
-      channel: this.props.channel,
-      username: this.props.user,
-      icon_emoji: this.props.emoji,
-      attachments: [
-        {
-          fallback: `Feedback (${selectedType})`,
-          author_name: this.props.user,
-          color: level,
-          title: selectedType,
-          title_link: document.location.href,
-          text: message,
-          footer: 'React Slack Feedback'
-        }
-      ]
+      hasPassedChallenge: false,
     };
-
-    // Attach the image (if available)
-    if (image.url) payload.attachments[0].image_url = image.url;
-
-    // Submit the payload
-    this.props.onSubmit.call(this, payload);
   }
 
-  attachImage(event) {
-
-    var { files } = event.target;
-
-    var file = files[0];
-    file.preview = window.URL.createObjectURL(file);
-
-    this.setState({
-      image: file,
-      uploadingImage: true
-    }, () => {
-      this.props.onImageUpload.call(this, file);
-    });
-  }
-
-  uploadError(err) {
-
-    this.setState({
-      uploading: false,
-      error: 'Error Uploading Image!'
-    }, () => {
-
-      this.removeImage();
-
-      setTimeout(() => {
-        this.setState({ error: null });
-      }, 6 * 1000);
-    });
-  }
-
-  imageUploaded(url) {
-    if (typeof url !== 'string') {
-      console.error('[SlackFeedback] `url` argument in `imageUploaded` method must be a string');
-      this.removeImage();
-      return;
-    }
-
-    // Merge the image URL with the file object,
-    // the resulting object will contain only the preview and the URL.
-    // Any file information will be lost
-    var image = {...this.state.image, url};
-
-    this.setState({
-      uploadingImage: false,
-      image
-    });
-  }
-
-  renderImageUpload() {
-    if (this.state.image.preview) {
-      return this.renderImagePreview();
-    }
-
-    return (
-      <div class="SlackFeedback-image-upload">
-        <label class="SlackFeedback-image-upload-button" for="imageUpload">
-          {this.props.imageUploadText}
-        </label>
-
-        <input
-          id="imageUpload"
-          type="file"
-          accept="image/*"
-          style={{ display: 'none' }}
-          onChange={event => this.attachImage(event)}
-        />
-      </div>
-    );
-  }
-
-  removeImage(event) {
-    if (event) event.preventDefault();
-
-    this.setState({
-      image: {},
-      uploadingImage: false
-    });
-  }
-
-  renderImagePreview() {
-    var { image, uploadingImage } = this.state;
-
-    if (!image.preview) return null;
-
-    return (
-      <div class="SlackFeedback--image-preview" style={{
-          backgroundImage: `url(${image.preview})`
-        }}>
-        {uploadingImage ?
-          <div class="SlackFeedback--loader"></div> :
-          <div class="SlackFeedback--preview-overlay">
-            <span onClick={this.removeImage}>Remove</span>
-          </div>
-        }
-      </div>
-    );
+  componentDidMount = () => {
+    const grecaptchaInterval = setInterval(() => {
+      if(typeof window !== 'undefined' && typeof window.grecaptcha !== 'undefined'){
+        this.renderReCAPTCHA()
+        clearInterval(grecaptchaInterval)
+      }
+    }, 1000)
   }
 
   render() {
-    var {
+    const {
       active,
       sending,
       sent,
@@ -308,7 +78,7 @@ class SlackFeedback extends Component {
       uploadingImage
     } = this.state;
 
-    var submitText = 'Send Feedback';
+    let submitText = 'Send Feedback';
 
     if (sent) submitText = 'Sent!';
     if (sending && !sent) submitText = 'Sending Feedback...';
@@ -335,13 +105,16 @@ class SlackFeedback extends Component {
             <div class="close" onClick={this.close}>close</div>
           </div>
 
-          <div class="SlackFeedback--content">
+          <form
+            class="SlackFeedback--content"
+            onSubmit={this.send}
+          >
 
             <label class="SlackFeedback--label">Name</label>
             <input class="SlackFeedback--input" ref="name" />
 
             <label class="SlackFeedback--label">Email</label>
-            <input class="SlackFeedback--input" ref="email" />
+            <input class="SlackFeedback--input" ref="email" required />
 
             <label class="SlackFeedback--label">Feedback Type</label>
             <ul class="SlackFeedback--tabs">
@@ -362,26 +135,21 @@ class SlackFeedback extends Component {
               class="SlackFeedback--textarea"
               placeholder="Message..."
               rows={6}
+              required
             />
 
             {/* Only render the image upload if there's callback available  */}
             {this.props.onImageUpload ? this.renderImageUpload() : null}
 
-            <div
-              ref={ref => this.grecaptcha = ref}
-              class="g-recaptcha"
-              data-sitekey="6LfHbhwUAAAAALhe7fMSRu_G1U0tmw8MvLKub4PZ"
-              style={{
-                width: '100%',
-              }}
-            />
+            <div id="g-recaptcha" />
 
             <button
               class={classNames('submit', { sent, error, disabled: sending || uploadingImage })}
-              onClick={this.send}>
+              type="submit"
+            >
               {submitText}
             </button>
-          </div>
+          </form>
 
         </div>
 
@@ -391,6 +159,264 @@ class SlackFeedback extends Component {
           onClick={this.toggle}>
           <SlackIcon /> {this.props.buttonText}
         </div>
+      </div>
+    );
+  }
+
+  renderReCAPTCHA = () => {
+    grecaptcha.render('g-recaptcha', {
+      sitekey: this.props.sitekey,
+      callback: this.verifyReCAPTCHA
+    })
+  }
+
+  verifyReCAPTCHA = () => {
+    this.setState({ hasPassedChallenge: true })
+  }
+
+  toggle = () => {
+    if (this.state.active) {
+      this.close();
+    } else {
+      this.activate();
+    }
+  }
+
+  activate = () => {
+    this.setState({
+      active: !this.state.active
+    });
+
+    document.addEventListener('click', this.handleClickOutside.bind(this));
+  }
+
+  handleClickOutside = (event) => {
+
+    if (event.defaultPrevented) return;
+
+    if (!this.refs.SlackFeedback.contains(event.target)) {
+      this.close();
+    }
+  }
+
+  close = () => {
+    this.setState({
+      active: false
+    });
+
+    document.removeEventListener('click', this.handleClickOutside.bind(this));
+  }
+
+  toggleSendURL = () => {
+    this.setState({
+      sendURL: !this.state.sendURL
+    });
+  }
+
+  selectType = (e) => {
+    this.setState({
+      selectedType: e.target.innerText
+    });
+  }
+
+  sent = () => {
+    this.setState({
+      sending: false,
+      sent: true,
+      image: {},
+      error: false,
+      hasPassedChallenge: false,
+    }, () => {
+      this.refs.name.value = '';
+      this.refs.email.value = '';
+      this.refs.message.value = '';
+      setTimeout(() => {
+        this.setState({ sent: false });
+      }, 5 * 1000);
+    });
+  }
+
+  error = (err) => {
+    this.setState({
+      sending: false,
+      error: this.determineErrorType(err)
+    }, () => {
+
+      setTimeout(() => {
+        this.setState({ error: null })
+      }, 8 * 1000);
+    });
+  }
+
+  determineErrorType = (err) => {
+    if (!err || typeof err !== 'string') return 'Unexpected Error!';
+
+    switch(err.status) {
+      case 400: return 'Bad Request!';
+      case 403: return 'Forbidden!';
+      case 404: return 'Channel Not Found!';
+      case 410: return 'Channel is Archived!';
+      case 500: return 'Server Error!';
+      default: return 'Unexpected Error!';
+    }
+  }
+
+  send = (event) => {
+    event.preventDefault()
+
+    if(!grecaptcha.getResponse()) {
+      alert('Oh no, you forgot to solve the CAPTCHA! Please try again.')
+      return
+    }
+
+    const { selectedType, image } = this.state;
+    let name = this.refs.name.value;
+    let email = this.refs.email.value;
+    let message = this.refs.message.value;
+    let level;
+
+    this.setState({
+      sending: true
+    });
+
+    message = `
+      *Name*: ${name}
+      \n *Email*: ${email}
+      \n *Message*: ${message}
+      \n <${document.location.href}>
+    `;
+
+    // Slack accepts 3 color levels: danger (red), good (green) and warning (orange)
+    switch (selectedType) {
+      case 'Bug':
+        level = 'danger';
+        break;
+      case 'Feature':
+        level = 'good';
+        break;
+      case 'Improvement':
+        level = 'warning';
+        break;
+    }
+
+    const payload = {
+      channel: this.props.channel,
+      username: this.props.user,
+      icon_emoji: this.props.emoji,
+      attachments: [
+        {
+          fallback: `Feedback (${selectedType})`,
+          author_name: this.props.user,
+          color: level,
+          title: selectedType,
+          title_link: document.location.href,
+          text: message,
+          footer: 'React Slack Feedback'
+        }
+      ]
+    };
+
+    // Attach the image (if available)
+    if (image.url) payload.attachments[0].image_url = image.url;
+
+    // Submit the payload
+    this.props.onSubmit.call(this, payload);
+  }
+
+  attachImage = (event) => {
+
+    const { files } = event.target;
+
+    const file = files[0];
+    file.preview = window.URL.createObjectURL(file);
+
+    this.setState({
+      image: file,
+      uploadingImage: true
+    }, () => {
+      this.props.onImageUpload.call(this, file);
+    });
+  }
+
+  uploadError = (err) => {
+
+    this.setState({
+      uploading: false,
+      error: 'Error Uploading Image!'
+    }, () => {
+
+      this.removeImage();
+
+      setTimeout(() => {
+        this.setState({ error: null });
+      }, 6 * 1000);
+    });
+  }
+
+  imageUploaded = (url) => {
+    if (typeof url !== 'string') {
+      console.error('[SlackFeedback] `url` argument in `imageUploaded` method must be a string');
+      this.removeImage();
+      return;
+    }
+
+    // Merge the image URL with the file object,
+    // the resulting object will contain only the preview and the URL.
+    // Any file information will be lost
+    const image = {...this.state.image, url};
+
+    this.setState({
+      uploadingImage: false,
+      image
+    });
+  }
+
+  renderImageUpload = () => {
+    if (this.state.image.preview) {
+      return this.renderImagePreview();
+    }
+
+    return (
+      <div class="SlackFeedback-image-upload">
+        <label class="SlackFeedback-image-upload-button" for="imageUpload">
+          {this.props.imageUploadText}
+        </label>
+
+        <input
+          id="imageUpload"
+          type="file"
+          accept="image/*"
+          style={{ display: 'none' }}
+          onChange={event => this.attachImage(event)}
+        />
+      </div>
+    );
+  }
+
+  removeImage = (event) => {
+    if (event) event.preventDefault();
+
+    this.setState({
+      image: {},
+      uploadingImage: false
+    });
+  }
+
+  renderImagePreview = () => {
+    const { image, uploadingImage } = this.state;
+
+    if (!image.preview) return null;
+
+    return (
+      <div class="SlackFeedback--image-preview" style={{
+          backgroundImage: `url(${image.preview})`
+        }}>
+        {uploadingImage ?
+          <div class="SlackFeedback--loader"></div> :
+          <div class="SlackFeedback--preview-overlay">
+            <span onClick={this.removeImage}>Remove</span>
+          </div>
+        }
       </div>
     );
   }
